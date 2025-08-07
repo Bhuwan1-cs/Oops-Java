9eaf279e4624dea1b0d10dd77dfad75cb7a9e8b4
Bhuwan1-cs <bhuwan.kumar.cs28@iilm.edu>
2025-08-07 11:36:50 +0530

    Add files via upload

---
+++ Add_multiple.java
@@ -0,0 +1,7 @@
+public class Add_multiple {
+    public static void main(String[] args) {
+        System.out.println("Hari prasad");
+        System.out.println("123 Street");
+        System.out.println("Delhi");
+    }
+}
