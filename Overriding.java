fd7252a2d1f5533213402fe7407ec263213dc334
Bhuwan1-cs <bhuwan.kumar.cs28@iilm.edu>
2025-11-20 13:00:54 +0530

    Create Overriding.java

---
+++ Overriding.java
@@ -0,0 +1,17 @@
+class Animal {
+    public void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+class Dog extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+}
+public class OverrideDemo {
+    public static void main(String[] args) {
+        Animal obj = new Dog();
+        obj.sound();              
+    }
+}
