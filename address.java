eb8f4e279fe6b918c878882ec9cfa394c95746a5
Bhuwan1-cs <bhuwan.kumar.cs28@iilm.edu>
2025-11-20 12:39:22 +0530

    Create  address.java

---
+++ address.java
@@ -0,0 +1,10 @@
+ public class PrintAddress {
+  public static void main(String[] args) {
+  // Using single print
+  System.out.print(""Name: John Doe, Address: 123 Java Lane"");
+  
+  // Using multiple println
+  System.out.println(""Name: John Doe"");
+  System.out.println(""Address: 123 Java Lane"");
+  }
+ }"
