4c070a5fcf52969cbfc42000ead92ae9630aec07
Bhuwan1-cs <bhuwan.kumar.cs28@iilm.edu>
2025-11-20 12:52:24 +0530

    Create meters converts.java

---
+++ meters converts.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MeterToFeet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value in meters: ");
+        double meters = sc.nextDouble();
+
+        double feet = meters * 3.28084;
+
+        System.out.println("Value in feet: " + feet);
+
+        sc.close();
+    }
+}
