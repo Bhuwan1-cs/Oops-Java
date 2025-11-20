6933832ce3d20ca403e46d05dd01635e18dd1fb3
Bhuwan1-cs <bhuwan.kumar.cs28@iilm.edu>
2025-11-20 13:03:29 +0530

    Create User_Define_Package.java

---
+++ User_Define_Package.java
@@ -0,0 +1,12 @@
+package mypackage;
+public class MyClass {
+    public int x = 10;
+}
+
+import mypackage.MyClass;
+public class PackageDemo {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        System.out.println("Value from package: " + obj.x);
+    }
+}
